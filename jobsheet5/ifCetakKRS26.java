28f80cc3c3279298d368f79a90d8f736d39fe909
l4utan <espressvre29@gmail.com>
2025-10-02 14:21:48 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS26.java
+++ jobsheet5/ifCetakKRS26.java
@@ -10,11 +10,7 @@ public class ifCetakKRS26 {
         System.out.println("Apakah UKT sudah lunas? (true/false)");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        }else{
-            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-        }
+        String hasil = (uktLunas) ? "Pembayaran UKT terverifikasi \nSilahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+        System.out.println(hasil);
     }
 }
