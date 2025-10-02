08c4c61aa97414a76e5bdc22ce28c2be186b6a6c
l4utan <espressvre29@gmail.com>
2025-10-02 14:36:12 +0700

    Tugas 2

---
+++ jobsheet5/Tugas2.java
@@ -0,0 +1,18 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan angka: ");
+        int angka = sc.nextInt();
+
+        if (angka%2 == 0) {
+            System.out.println("Angka kamu genap");
+        }else{
+            System.out.println("Angka kamu ganjil");
+        }
+    }
+}
