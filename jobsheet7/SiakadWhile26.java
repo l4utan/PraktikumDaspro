bc790dce53e8e3c3da7119f73017dec4c39afb71
l4utan <espressvre29@gmail.com>
2025-10-23 11:43:22 +0700

    Percobaan 2

--- jobsheet7/SiakadWhile26.java
+++ jobsheet7/SiakadWhile26.java
@@ -17,8 +17,7 @@ public class SiakadWhile26 {
             }
             
             if (nilai > 80 && nilai <= 100) {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
-                System.out.println("Bagus, pertahankan nilanya");
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");    
             }else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah B+");
             }else if (nilai > 65 && nilai <= 73) {
