3206fef3a75d2d7ffd76bc691d7d4750b1d3bbe9
l4utan <espressvre29@gmail.com>
2025-11-12 18:19:50 +0700

    Modifikasi Percobaan 3

--- jobsheet9/ArrayRataNilai26.java
+++ jobsheet9/ArrayRataNilai26.java
@@ -4,9 +4,17 @@ public class ArrayRataNilai26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
+        int[] nilaiMhs;
         double total = 0;
         double rata2;
+        int mhsLulus = 0, mhsGagal = 0;
+        int banyakMhs;
+        double lulus = 0, gagal = 0;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        banyakMhs = sc.nextInt();
+
+        nilaiMhs = new int[banyakMhs];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
@@ -19,5 +27,21 @@ public class ArrayRataNilai26 {
 
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = " + rata2);
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                mhsLulus ++;
+                lulus += nilaiMhs[i];
+            }else if (nilaiMhs[i] <= 70) {
+                mhsGagal ++;
+                gagal += nilaiMhs[i];
+            }
+        }
+        
+        double rataLulus = lulus/mhsLulus;
+        double rataGagal = gagal/mhsGagal;
+
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataGagal);
     }
 }
