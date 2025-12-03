e79959486d7fda0fd1cceccc579e3998bef08516
l4utan <espressvre29@gmail.com>
2025-12-03 18:59:05 +0700

    Modifikasi Percobaan 2

--- jobsheet11/Kafe26.java
+++ jobsheet11/Kafe26.java
@@ -1,10 +1,18 @@
 public class Kafe26 {
 
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kamu mendapatkan tambahan diskon sebesar 50%");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kamu mendapatkan tambahan diskon sebesar 30%");
+        } else{
+            System.out.println("Kode Invalid!");
         }
         
         System.out.println("===== MENU RESTO KAFE =====");
@@ -19,6 +27,6 @@ public class Kafe26 {
     }
 
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON30");
     }
 }
