a590d28da0181db4f9aefa19542d22e8caa9ee1d
l4utan <espressvre29@gmail.com>
2025-12-03 18:07:18 +0700

    Percobaan 1

---
+++ jobsheet11/Kafe26.java
@@ -0,0 +1,18 @@
+public class Kafe26 {
+
+    public static void Menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,0000");
+        System.out.println("2. Cappucino - Rp 20,0000");
+        System.out.println("3. Latte - Rp 22,0000");
+        System.out.println("4. Teh Tarik - Rp 12,0000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("============================");
+        System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+
+    public static void main(String[] args) {
+        Menu();
+    }
+}
