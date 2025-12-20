5b9be85a580b3e4e742debcbd923c64a9e2713ee
l4utan <espressvre29@gmail.com>
2025-12-20 18:48:43 +0700

    Modifikasi Percobaan 3

--- jobsheet11/Kafe26.java
+++ jobsheet11/Kafe26.java
@@ -2,20 +2,8 @@ import java.util.Scanner;
 
 public class Kafe26 {
 
-    public static void Menu() {
-        // System.out.println("Selamat datang, " + namaPelanggan + "!");
-
-        // if (isMember) {
-        //     System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        // }
-
-        // if (kodePromo.equals("DISKON50")) {
-        //     System.out.println("Kamu mendapatkan tambahan diskon sebesar 50%");
-        // } else if (kodePromo.equals("DISKON30")) {
-        //     System.out.println("Kamu mendapatkan tambahan diskon sebesar 30%");
-        // } else{
-        //     System.out.println("Kode Invalid!");
-        // }
+    public static void Menu(String namaPelanggan) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
         
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,0000");
@@ -26,27 +14,67 @@ public class Kafe26 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("============================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
+        System.out.println("Ketik 0 jika ingin berhenti memesan");
     }
 
-    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+    public static double[] hitungTotalHarga26(int pilihanMenu, int banyakItem, String kodePromo) {
         int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        double diskon = 0;
 
-        int hargaTotal = hargaItems[pilihanMenu-1] * banyakItem;
-        return hargaTotal;
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kamu mendapatkan tambahan diskon sebesar 50%");
+            diskon = 0.5;
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kamu mendapatkan tambahan diskon sebesar 30%");
+            diskon = 0.3;
+        } else if(!kodePromo.isEmpty()){
+            System.out.println("Kode Invalid!");
+        }
+        
+        double harga = hargaItems[pilihanMenu-1] * banyakItem;
+        double hargaDiskon = harga * diskon;
+        double hargaTotal = harga - hargaDiskon;
+        return new double[] {hargaTotal, hargaDiskon, harga};
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan nama kamu: ");
+        String namaPelanggan = sc.nextLine();
 
-        Menu();
+        Menu(namaPelanggan);
+        int lanjut = 0;
+        double grandTot = 0;
+        double totalDiskon = 0;
+        double totalSebelumDiskon = 0;
+        System.out.print("Masukkan kode diskon yang anda punya: ");
+        String kodePromo = sc.nextLine();
 
-        System.out.print("\nMasukkan nomor menu yang ingin anda pesan: ");
-        int pilihanMenu = sc.nextInt();
-        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
-        int banyakItem = sc.nextInt();
+        do {
+            System.out.print("\nMasukkan nomor menu yang ingin anda pesan: ");
+            int pilihanMenu = sc.nextInt();
+            System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+            int banyakItem = sc.nextInt();
 
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+            double[] hasil = hitungTotalHarga26(pilihanMenu, banyakItem, kodePromo);
 
-        System.out.println("Total harga untuk pesanan anda: Rp." + totalHarga);
+            grandTot += hasil[0];
+            totalDiskon += hasil[1];
+            totalSebelumDiskon += hasil[2];
+
+            sc.nextLine();
+            System.out.print("Tekan 0 jika ingin keluar: ");
+            lanjut = sc.nextInt();
+            sc.nextLine();
+            
+        } while (lanjut != 0);
+        
+        
+
+
+        System.out.println("Total harga sebelum diskon: Rp." + totalSebelumDiskon);
+        System.out.println("Total diskon yang anda dapat: Rp." + totalDiskon);
+        System.out.println("Total harga yang anda pesan: Rp." + grandTot);
     }
 }
